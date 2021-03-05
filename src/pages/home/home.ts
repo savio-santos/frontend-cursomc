40a22adc489cf8859d968fef04a8c7f22a3aada7
Sávio Santos <saviosantos276@gmail.com>
2021-03-05 14:38:56 -0300

    obtendo dados do formulario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credencias.dto';
 
 @IonicPage()
 @Component({
@@ -7,6 +8,11 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds : CredenciaisDTO ={
+    email : "",
+    senha :""
+  };
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
@@ -20,6 +26,7 @@ export class HomePage {
 
 
   login() {
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage') // *laze loadind navegando pelo nome da pagina (string) 
 
   }
