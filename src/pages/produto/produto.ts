98996361083267fae90300f9c6ceb46926fb89ac
Sávio Santos <saviosantos276@gmail.com>
2021-03-26 11:30:11 -0300

    carregando imgs do produto

--- src/pages/produto/produto.ts
+++ src/pages/produto/produto.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -23,11 +24,23 @@ export class ProdutoPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
         error => { }
       );
   }
 
+  loadImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imgUrl = `${API_CONFIG.buketBaseUrl}/prod${item.id}-small.jpg`;
+
+        }, error => { }
+        );
+    }
+  }
 }
 
 
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
@@ -10,5 +11,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id:string) : Observable<any>{
+        let url = `${API_CONFIG.buketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url,{responseType:'blob'});
     }
 }
